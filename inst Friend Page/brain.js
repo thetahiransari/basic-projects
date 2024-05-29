2fbd579921b2d232e76fd04534c1cb7ccd966f27
Tahir Asif <159940732+thetahiransari@users.noreply.github.com>
2024-05-29 14:48:01 +0530

    Add files via upload

---
+++ inst Friend Page/brain.js
@@ -0,0 +1,10 @@
+var stop = document.querySelector("h5")
+
+var addfriend = document.querySelector(" #add")
+
+
+
+addfriend.addEventListener("click", function(){
+   stop.innerHTML = " Friend "
+   stop.style.color = "blue"
+});
